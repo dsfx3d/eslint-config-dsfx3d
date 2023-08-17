10372090396b70100c9d4641731cb778a8e63bac
Yashodhan Singh Rathore <dsfx3d@gmail.com>
2023-08-18 00:10:40 +0530

    feat: :sparkles: expose ESLint config

---
+++ index.js
@@ -0,0 +1,24 @@
+/**
+ * @type {import('eslint').Linter.Config}
+ */
+const config = {
+  extends: [
+    "eslint:recommended",
+    "plugin:prettier/recommended",
+    "eslint-config-unjs",
+  ],
+  plugins: ["sort-imports-es6-autofix"],
+  rules: {
+    "sort-imports-es6-autofix/sort-imports-es6": [
+      2,
+      {
+        ignoreCase: false,
+        ignoreMemberSort: false,
+        memberSyntaxSortOrder: ["none", "all", "multiple", "single"],
+      },
+    ],
+  },
+};
+
+// eslint-disable-next-line unicorn/prefer-module
+module.exports = config;
